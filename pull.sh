50a8842636c9a64b463ab8539577237f62b386b9
Alain <alain.seys@outlook.com>
2024-07-16 12:41:53 +0200

    add pull.sh script to pull the changes that are add to git to the server

---
+++ pull.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# List of directories containing git repositories
+repositories=(
+    "/var/www/api"
+    "/var/www/doobeedoo.be"
+    "/var/www/noordzee105.be"
+    "/var/www/Phpmailer"
+    "/etc/apache2"
+    "/etc/icecast2"
+)
+
+# Function to update the repository
+update_repo() {
+    local repo_dir=$1
+
+    if [ -d "$repo_dir" ]; then
+        cd "$repo_dir" || return
+        if [ -d ".git" ]; then
+            echo "Updating repository in $repo_dir"
+            git pull
+        else
+            echo "No git repository found in $repo_dir"
+        fi
+    else
+        echo "Directory $repo_dir does not exist"
+    fi
+}
+
+# Loop through each repository and update it
+for repo in "${repositories[@]}"; do
+    update_repo "$repo"
+done
